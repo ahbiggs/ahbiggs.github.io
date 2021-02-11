b37b6941d356e3d8a89a8d325c3e72886ff2bd12
ahbiggs <ahwadsworth@gmail.com>
2021-02-10 20:11:00 -0700

    update

--- Lesson04/sripts.js
+++ Lesson04/sripts.js
@@ -13,3 +13,80 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 
   classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
   */
+ let year = new Date().getFullYear();
+let day = new Date().getDay();
+let dayNum = new Date().getDate();
+let month = new Date().getMonth();
+
+
+if (month == 0) {
+    month = "January"
+}
+if (month == 1) {
+    month = "February"
+}
+if (month == 2) {
+    month = "March"
+}
+if (month == 3) {
+    month = "April"
+}
+if (month == 4) {
+    month = "May"
+}
+if (month == 5) {
+    month = "June"
+}
+if (month == 6) {
+    month = "July"
+}
+if (month == 7) {
+    month = "August"
+}
+if (month == 8) {
+    month = "September"
+}
+if (month == 9) {
+    month = "October"
+}
+if (month == 10) {
+    month = "November"
+}
+if (month == 11) {
+    month = "December"
+}
+
+
+
+if (day == 1) {
+    day = "Monday";
+}
+if (day == 2) {
+    day = "Tuesday";
+}
+if (day == 3) {
+    day = "Wednesday";
+}
+if (day == 4) {
+    day = "Thursday";
+}
+if (day == 5) {
+    day = "Friday";
+}
+if (day == 6) {
+    day = "Saturday";
+}
+if (day == 7) {
+    day = "Sunday";
+}
+
+document.getElementById("year").innerHTML = "&#169; " + year + "AnDee Biggs";
+document.getElementById("output").innerHTML = day + ', ' + dayNum + ' ' + month + ' ' + year;
+
+if (day == "Friday") {
+    document.getElementById("announcement").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+}
+
+if (day != "Friday") {
+    document.getElementById("announcement").style.display = 'none';
+}
