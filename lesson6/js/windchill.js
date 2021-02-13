6c68a69843296a50bd495d3c8f31d0948ab8efd3
ahbiggs <ahwadsworth@gmail.com>
2021-02-13 13:48:40 -0700

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -4,7 +4,7 @@ console.log(temperature);
 console.log(windSpeed);
 
 if (temperature <= 50) {
-    var windChill = Math.round(35.74 + (0.6215 * 76) - (35.75 * Math.pow(5, 0.16)) + (0.4275 * 76 * Math.pow(5, 0.16)));
+    var windChill = Math.round(35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temperature * Math.pow(windspeed, 0.16)));
     document.getElementById("windchill").innerHTML = windChill + "&#176; F";
 }
 
