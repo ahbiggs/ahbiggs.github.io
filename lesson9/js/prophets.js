c0f041eaf48a2eaca49539a5d71b592324c53ae5
ahbiggs <ahwadsworth@gmail.com>
2021-03-03 20:31:10 -0700

    update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -12,17 +12,14 @@ fetch(requestURL)
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let p = document.createElement('p');
-        let p1 = document.createElement('p1');
         let image = document.createElement('img');
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p.textContent = 'Date of Birth: ' + prophets[i].birthdate 
-        p1.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        p.textContent = 'Date of Birth: ' + prophets[i].birthdate + ' ' + 'Place of Birth: ' + prophets[i].birthplace;
         image.setAttribute('src', prophets[i].imageurl);
 
         card.appendChild(h2);
         card.appendChild(p);
-        card.appendChild(p1);
         card.appendChild(image);
         
 
@@ -30,4 +27,5 @@ fetch(requestURL)
 
     }
 
-});
+    
+  });
