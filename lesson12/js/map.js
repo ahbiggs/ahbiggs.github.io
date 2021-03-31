27d50b0ad858518b321e6d4abeb173573a2866a4
ahbiggs <ahwadsworth@gmail.com>
2021-03-31 10:28:33 -0600

    update

--- lesson12/js/map.js
+++ lesson12/js/map.js
@@ -1,10 +1,15 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    var uluru = {lat: 40.5247, lng: -111.8638};
+    const uluru = { lat: -25.344, lng: 131.036 };
     // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 13, center: uluru});
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
   }
