1e29463f482c4518467ada296e2fbe3a7603fe32
ahbiggs <ahwadsworth@gmail.com>
2021-03-31 10:45:34 -0600

    update

--- lesson12/js/map.js
+++ lesson12/js/map.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.036 };
+    const uluru = { lat: 40.5247, lng: -111.8638 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
