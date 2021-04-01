b4e41c664c9f881cd380ea2e628297f7968d7d6b
ahbiggs <ahwadsworth@gmail.com>
2021-04-01 11:32:28 -0600

    update

--- lesson12/js/business.js
+++ lesson12/js/business.js
@@ -8,7 +8,7 @@ fetch(requestURL)
     console.table(jsonObject);
     const business = jsonObject['business'];
 
-     {
+for (let i = 0; i < business.name; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let p = document.createElement('p');
@@ -16,7 +16,7 @@ fetch(requestURL)
 
         h2.textContent = business[i].name + ' ' + business[i].phone;
         p.textContent = 'Address: ' + business[i].address + ' ' + 'Website: ' + business[i].website;
-        image.setAttribute('src', prophets[i].logolocation);
+        image.setAttribute('src', business[i].logolocation);
 
         card.appendChild(h2);
         card.appendChild(p);
