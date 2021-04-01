84d9e2690e92ef501d5d0f52e95c2173e74f3ebf
ahbiggs <ahwadsworth@gmail.com>
2021-04-01 13:35:32 -0600

    update

--- lesson12/js/business.js
+++ lesson12/js/business.js
@@ -6,17 +6,17 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
-    const business = jsonObject['business'];
+    const prophets = jsonObject['prophets'];
 
-for (let i = 0; i < business.name; i++ ) {
+    for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let p = document.createElement('p');
         let image = document.createElement('img');
 
-        h2.textContent = business[i].name + ' ' + business[i].phone;
-        p.textContent = 'Address: ' + business[i].address + ' ' + 'Website: ' + business[i].website;
-        image.setAttribute('src', business[i].logolocation);
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        p.textContent = 'Date of Birth: ' + prophets[i].birthdate + ' ' + 'Place of Birth: ' + prophets[i].birthplace;
+        image.setAttribute('src', prophets[i].imageurl);
 
         card.appendChild(h2);
         card.appendChild(p);
